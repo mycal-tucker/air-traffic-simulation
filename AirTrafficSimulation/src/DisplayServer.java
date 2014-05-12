bf2fe145ccfe097757d8b7eed9e0bc30900d49b5
CosmeOchoa <cosme@mit.edu>
2014-05-11 23:20:37 -0400

    Fixed Display Server

--- AirTrafficSimulation/src/DisplayServer.java
+++ AirTrafficSimulation/src/DisplayServer.java
@@ -293,7 +293,8 @@ public class DisplayServer extends JPanel implements KeyListener {
 				drawY[i] = 100*gain- drawY[i];
 			}
 			g.drawPolygon(drawX, drawY, 9);
-		
+			
+			
 			if (fuel[j]>100)
 				g.setColor(Color.green);
 			else if(fuel[j]<=100 && fuel[j]>50)
@@ -302,6 +303,7 @@ public class DisplayServer extends JPanel implements KeyListener {
 				g.setColor(Color.orange);
 			else
 				g.setColor(Color.red);
+			
 			g.fillPolygon(drawX, drawY, 9);
 		}
 
